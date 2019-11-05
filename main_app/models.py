fb321ee132938829c877a1c66c725c4442fc459b
Laura Sora <laurasora@Lauras-MacBook-Pro.local>
2019-11-04 22:36:23 -0500

    Add model, views, urls, templates for events

--- main_app/models.py
+++ main_app/models.py
@@ -2,3 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    address = models.TextField(max_length=250)
+    date_time = models.DateTimeField('event date')
+    occasion = models.TextField(max_length=150)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE) 
